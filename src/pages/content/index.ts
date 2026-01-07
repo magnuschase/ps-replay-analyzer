96200e8f1af7c459e2742ba4f580a5c5cb526317
Jakub Kapała <jakub@kapala.xyz>
2026-01-07 16:58:42 +0100

    feat: remove react from content script

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,21 +1,7 @@
-import { createRoot } from "react-dom/client";
 import "./style.css";
 // @crxjs/vite-plugin forces vite to build ?script files as standalone scripts
 // import will resolve to something like "/assets/injected.28c29.js"
 import injectedScript from "./injected?script";
-
-const div = document.createElement("div");
-div.id = "__root";
-document.body.appendChild(div);
-
-const rootContainer = document.querySelector("#__root");
-if (!rootContainer) throw new Error("Can't find Content root element");
-const root = createRoot(rootContainer);
-root.render(
-  <div className="absolute bottom-0 left-0 text-lg text-black bg-amber-400 z-50">
-    content script <span className="your-class">loaded</span>
-  </div>
-);
 
 try {
   console.log("[PS Replay Analyzer] content script loaded");
